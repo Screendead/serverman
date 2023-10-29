4bddc2b346cbb955cdeaefdcbd6a526d64491c89
Jack <jack@lusher.digital>
2023-10-29 12:15:15 +0000

    Wrap app with StreamProvider for Droplet instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 import 'package:serverman/firebase_options.dart';
+import 'package:serverman/models/droplet.dart';
+import 'package:serverman/services/droplet_service.dart';
 import 'package:serverman/widgets/droplet_manager.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -18,22 +22,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Server Manager',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-          body: Center(
-        child: ListView(
-          children: <Widget>[
-            DropletManager(
-              dropletId: dotenv.env['DIGITALOCEAN_DROPLET_ID'] as String,
-            ),
-          ],
+    return StreamProvider<Droplet?>.value(
+      value: GetIt.instance.get<DropletService>().dropletStream,
+      initialData: null,
+      updateShouldNotify: (Droplet? previous, Droplet? current) {
+        return previous != current;
+      },
+      child: MaterialApp(
+        title: 'Server Manager',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      )),
+        home: Scaffold(
+          body: Center(
+            child: ListView(
+              children: <Widget>[
+                DropletManager(
+                  dropletId: dotenv.env['DIGITALOCEAN_DROPLET_ID'] as String,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
